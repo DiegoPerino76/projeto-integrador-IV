6874cbba5d689b3d7b9a772ca3c1e7c2f6c21462
DiegoPerino76 <271780@unifio.edu.br>
2025-08-26 23:46:10 +0000

    aula 1

---
+++ projeto-integrador-IV/src/app/app-module.ts
@@ -0,0 +1,23 @@
+import { NgModule, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing-module';
+import { App } from './app';
+import { Soma } from './soma/soma';
+
+@NgModule({
+  declarations: [
+    App,
+    Soma
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection()
+  ],
+  bootstrap: [App]
+})
+export class AppModule { }
